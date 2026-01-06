0d2cfc284f55210274dda92aa27e605687dabb04
Dasunpriya-R <dasunpriya2004@gmail.com>
2026-01-06 22:11:52 +0530

    Add files via upload

---
+++ LinkedListFIFOQueue.java
@@ -0,0 +1,49 @@
+public class LinkedListFIFOQueue {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node front = null, rear = null;
+
+    void enqueue(int data) {
+        Node newNode = new Node(data);
+
+        if (rear == null) {
+            front = rear = newNode;
+            return;
+        }
+
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    int dequeue() {
+        if (front == null) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        int value = front.data;
+        front = front.next;
+
+        if (front == null) {
+            rear = null;
+        }
+
+        return value;
+    }
+
+    public static void main(String[] args) {
+        LinkedListFIFOQueue q = new LinkedListFIFOQueue();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        System.out.println(q.dequeue());
+    }
+}
