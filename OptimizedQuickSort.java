666dd9a40a2f78259d51d81128a5bb64133d0d53
Dasunpriya-R <dasunpriya2004@gmail.com>
2026-01-26 00:00:20 +0530

    Add files via upload

---
+++ OptimizedQuickSort.java
@@ -0,0 +1,37 @@
+public class OptimizedQuickSort {
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low >= high)
+            return;
+
+        int pivot = arr[(low + high) / 2];
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (arr[i] < pivot) i++;
+            while (arr[j] > pivot) j--;
+
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSort(arr, low, j);
+        if (i < high)
+            quickSort(arr, i, high);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
